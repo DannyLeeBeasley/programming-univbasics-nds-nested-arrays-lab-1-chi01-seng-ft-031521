6cc006ee1964ac9a88ad896d2a759b2d8c6f97c6
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-05 20:37:28 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,10 +25,9 @@ end
 
 def sorted_matrix
   produce_storage_room = [
-    CONVENTIONAL_PRODUCE,
-    ORGANIC_PRODUCE
+    CONVENTIONAL_PRODUCE.sort,
+    ORGANIC_PRODUCE.sort
     ]
-    produce_storage_room.sort
 end
 
 def matrix_lookup(matrix, row, column)
